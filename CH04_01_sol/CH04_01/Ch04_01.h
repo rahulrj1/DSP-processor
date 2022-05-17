d12977900408c84fe964bd8dd0db22b5800f65b8
Rahul <rahuljas2050@gmail.com>
2022-05-18 03:04:43 +0530

    Added code for chapter 4

---
+++ CH04_01_sol/CH04_01/Ch04_01.h
@@ -0,0 +1,6 @@
+#pragma once
+#include "YmmVal.h"
+
+// Ch04_01_fcpp.cpp
+extern void MathI16_Iavx2(YmmVal c[6], const YmmVal* a, const YmmVal* b);
+extern void MathI32_Iavx2(YmmVal c[6], const YmmVal* a, const YmmVal* b);
